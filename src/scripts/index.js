8ef40124b28fda4f3a43589af9c392c5ac958849
Amanda Pearce <amandagpearce@gmail.com>
2020-06-25 13:27:11 -0300

    Refactoring of index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,67 +8,68 @@ const input = document.getElementById('search');
 const errorOutput = document.querySelector('.error-output');
 const searchResults = document.querySelector('.search-results');
 
+const orderNumber = document.querySelector('.order-client .number');
+const orderName = document.querySelector('.order-client .name');
+const orderValue = document.querySelector('.order-value h2');
+const orderDate = document.querySelector('.order-date h2');
+const orderStatus = document.querySelector('.order-status h2');
+
 function getData() {
     const xHR = new XMLHttpRequest();
     xHR.open('get','../../dados.json',true);
     xHR.send();
-    xHR.onload = tracking;
+    xHR.onload = setTracking;
 };
 
-function tracking(){
-   button.addEventListener('click', retrievePurchase);
-   
-   const orderNumber = document.querySelector('.order-client .number');
-   const orderName = document.querySelector('.order-client .name');
-   const orderValue = document.querySelector('.order-value h2');
-   const orderDate = document.querySelector('.order-date h2');
-   const orderStatus = document.querySelector('.order-status h2');
-   
+function setTracking(){
    let response = JSON.parse(this.responseText);
-   //console.log(response);
-
-   function retrievePurchase (event){
-        const searchTerm = input.value;
-        errorOutput.classList.add('hidden');
-
-        if (searchTerm !== ''){
-            event.preventDefault();
-        };
-
-        let result = response.encomendas.filter(function (e) {
-            if(e.numero === searchTerm) {
-                let dateStr = e.data;
-                let year = dateStr.substring(0, 4);
-                let month = dateStr.substring(5, 7);
-                let day = dateStr.substring(8, 10);
-
-                if(e.entregue) {
-                    orderStatus.innerText = 'Entregue';
-                }
-                else{
-                    orderStatus.innerText = 'Entregar';
-                };
-                
-                orderNumber.innerText = e.cliente.id+' - ';
-                orderName.innerText = e.cliente.nome;
-                orderValue.innerText = 'R$ '+e.valor;
-                orderDate.innerText = day+'/'+month+'/'+year;
-
-                searchResults.classList.remove('hidden');
-                return e;
-            };       
-        });
-
-        if(!result.length){
-            errorOutput.innerHTML='<h2>Encomenda <br>não encontrada!<h2>';
-            errorOutput.innerHTML+='<h2>Procure novamente<h2>';
-            errorOutput.classList.remove('hidden');
-            searchResults.classList.add('hidden');
-        };   
-   };
-
+   console.log(response);
+   
+   button.addEventListener('click', ()=>{
+       retrievePurchase(event,response), false;
+    });
 };
 
+const retrievePurchase = (event,purchases) => {
+    //console.log(purchase);
+    const searchTerm = input.value;
+    errorOutput.classList.add('hidden');
+
+    if (searchTerm !== ''){
+        event.preventDefault();
+    };
+
+    let result = purchases.encomendas.filter(function (e) {
+        if(e.numero === searchTerm) {
+            let dateStr = e.data;
+            let year = dateStr.substring(0, 4);
+            let month = dateStr.substring(5, 7);
+            let day = dateStr.substring(8, 10);
+
+            if(e.entregue) {
+                orderStatus.innerText = 'Entregue';
+            }
+            else{
+                orderStatus.innerText = 'Entregar';
+            };
+            
+            orderNumber.innerText = e.cliente.id+' - ';
+            orderName.innerText = e.cliente.nome;
+            orderValue.innerText = 'R$ '+e.valor;
+            orderDate.innerText = day+'/'+month+'/'+year;
+
+            searchResults.classList.remove('hidden');
+            return e;
+        };       
+    });
+
+    if(!result.length){
+        errorOutput.innerHTML='<h2>Encomenda <br>não encontrada!<h2>';
+        errorOutput.innerHTML+='<h2>Procure novamente<h2>';
+        errorOutput.classList.remove('hidden');
+        searchResults.classList.add('hidden');
+    };   
+};
 
 
 
